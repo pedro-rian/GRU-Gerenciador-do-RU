1517bbe007c566887ef4ef10642c3f1a075bd5bd
Pedro Rian <49865204+Pleonasmo@users.noreply.github.com>
2024-03-20 10:27:26 -0300

    Base do projeto gerada pelo Spring Initializr

---
+++ src/main/java/br/com/ufrn/imd/gru/GruApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufrn.imd.gru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ufrn/imd/gru/GruApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufrn.imd.gru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
